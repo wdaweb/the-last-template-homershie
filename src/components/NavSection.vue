b2440630b4eeaf1c6dd3b23ad009574a65fc39f8
homershie <homershie@gmail.com>
2025-06-17 16:17:25 +0800

    Add some styles to navbar

--- src/components/NavSection.vue
+++ src/components/NavSection.vue
@@ -1,22 +1,33 @@
 <template>
-  <el-menu class="nav-section" mode="horizontal" :ellipsis="false" router>
-    <el-menu-item index="/">
-      <el-icon><HomeFilled /></el-icon>
-      <span>首頁</span>
-    </el-menu-item>
-    <el-menu-item index="/about">
-      <el-icon><InfoFilled /></el-icon>
-      <span>關於我們</span>
-    </el-menu-item>
-    <el-menu-item index="/contact">
-      <el-icon><Message /></el-icon>
-      <span>聯絡我們</span>
-    </el-menu-item>
-    <el-menu-item index="/feedback">
-      <el-icon><ChatDotRound /></el-icon>
-      <span>意見回饋</span>
-    </el-menu-item>
-  </el-menu>
+  <div class="nav-wrapper">
+    <div class="logo-area">
+      <!-- 這裡可以放 LOGO 圖片或文字 -->
+      <img
+        src="https://nexus.uiparadox.com/wp-content/uploads/2024/09/logo-4.png"
+        alt="Logo"
+        class="logo-img"
+      />
+      <!-- 若沒有 logo.png 可改成 <span>LOGO</span> -->
+    </div>
+    <el-menu class="nav-section" mode="horizontal" :ellipsis="false" router>
+      <el-menu-item index="/">
+        <el-icon><HomeFilled /></el-icon>
+        <span>首頁</span>
+      </el-menu-item>
+      <el-menu-item index="/about">
+        <el-icon><InfoFilled /></el-icon>
+        <span>關於我們</span>
+      </el-menu-item>
+      <el-menu-item index="/feedback">
+        <el-icon><ChatDotRound /></el-icon>
+        <span>意見回饋</span>
+      </el-menu-item>
+      <el-menu-item index="/contact">
+        <el-icon><Message /></el-icon>
+        <span>聯絡我們</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
 </template>
 
 <script setup>
@@ -24,11 +35,50 @@ import { HomeFilled, InfoFilled, Message, ChatDotRound } from '@element-plus/ico
 </script>
 
 <style scoped>
-.nav-section {
-  position: fixed;
-  top: 0;
-  left: 0;
+.nav-wrapper {
+  display: flex;
+  align-items: center;
   width: 100%;
-  z-index: 1000;
+  height: 98px;
+  background: #141414;
+}
+.logo-area {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding-left: 32px;
+  padding-right: 32px;
+}
+.logo-img {
+  height: 30px;
+  width: auto;
+}
+.nav-section {
+  margin-left: auto;
+  width: auto;
+  height: 98px;
+}
+
+.el-menu {
+  background-color: transparent;
+  border: none;
+}
+
+.el-menu-item {
+  color: #53627c;
+  background-color: transparent !important;
+  list-style: none !important;
+}
+.el-menu-item:hover {
+  background-color: transparent !important;
+  color: white !important;
+  list-style: none !important;
+}
+
+.el-menu--horizontal > .el-menu-item.is-active {
+  color: var(--el-color-primary) !important;
+  background-color: transparent !important;
+  list-style: none !important;
+  border-bottom: none !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import 'element-plus/dist/index.css'
+import '@/styles/reset.css'
+import '@/styles/global.css'
+import '@/styles/variables.css'
+import '@/styles/rwd.css'
 import '@/styles/main.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
