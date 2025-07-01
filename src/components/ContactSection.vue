65e257126ad55aa9dedbd59b72f9c5a159f8d1f8
homershie <homershie@gmail.com>
2025-07-01 16:32:03 +0800

    Update Contact

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,185 +1,185 @@
 <template>
   <el-container class="contact-section" id="contact">
     <el-main>
-      <el-row>
+      <el-row class="title-row">
         <el-col :span="24">
           <h2>Get in <span class="gradient-stroke-text">TOUCH</span></h2>
         </el-col>
       </el-row>
-      <el-row>
-        <el-col :span="24">
-          <div class="cmsmasters_row_margin cmsmasters_1212">
-            <div id="cmsmasters_column_k0bxql2by" class="cmsmasters_column one_half">
-              <div class="cmsmasters_column_inner">
-                <div class="cmsmasters_text">
-                  <h4>Contact Information</h4>
-                  <p>Your email address is safe with us. Required fields are noted with *</p>
-                  <div class="contact-information-wrapper">
-                    <div class="contact-block">
-                      <div id="cmsmasters_icon_slxpsfqzvs" class="cmsmasters_icon_wrap">
-                        <span class="cmsmasters_simple_icon cmsmasters-icon-phone"></span
-                        ><span class="cmsmasters_simple_icon_title"></span>
-                      </div>
-                      <div class="text-box">
-                        <h5>Phone Number</h5>
-                        <p>+001 123 456 789, +002 321 654 987</p>
-                      </div>
+      <el-row class="content-row">
+        <el-col :span="11">
+          <div id="cmsmasters_column_k0bxql2by" class="cmsmasters_column">
+            <div class="cmsmasters_column_inner">
+              <div class="cmsmasters_text">
+                <h4>Contact Information</h4>
+                <p>Your email address is safe with us. Required fields are noted with *</p>
+                <div class="contact-information-wrapper">
+                  <div class="contact-block">
+                    <div id="cmsmasters_icon_slxpsfqzvs" class="cmsmasters_icon_wrap">
+                      <span class="fa-solid fa-phone"></span>
+                      <span class="cmsmasters_simple_icon_title"></span>
                     </div>
-                    <div class="contact-block">
-                      <div id="cmsmasters_icon_6fe373ojdc" class="cmsmasters_icon_wrap">
-                        <span class="cmsmasters_simple_icon cmsmasters-icon-mail"></span
-                        ><span class="cmsmasters_simple_icon_title"></span>
-                      </div>
-                      <div class="text-box">
-                        <h5>Email Address:</h5>
-                        <p>exampleinfo@gmail.com</p>
-                      </div>
-                    </div>
-                    <div class="contact-block">
-                      <div id="cmsmasters_icon_7pu6fe3fms" class="cmsmasters_icon_wrap">
-                        <span class="cmsmasters_simple_icon cmsmasters-icon-location-3"></span
-                        ><span class="cmsmasters_simple_icon_title"></span>
-                      </div>
-                      <div class="text-box">
-                        <h5>Address:</h5>
-                        <p>26 South Street, New York, NY 98, USA.</p>
-                        <p>&nbsp;</p>
-                      </div>
+                    <div class="text-box">
+                      <h5>Phone Number</h5>
+                      <p>+001 123 456 789, +002 321 654 987</p>
                     </div>
                   </div>
-                </div>
-                <div
-                  id="cmsmasters_heading_zeqwe011o"
-                  class="cmsmasters_heading_wrap cmsmasters_heading_align_left"
-                >
-                  <h5 class="cmsmasters_heading">Social Media:</h5>
-                </div>
-                <div class="cmsmasters_sharing">
-                  <div class="share_wrap">
-                    <a
-                      data-pin-do="buttonBookmark"
-                      data-pin-custom="true"
-                      class="button cmsmasters_pinterest_button cmsmasters-icon-pinterest"
-                      data-pin-log="button_pinit_bookmarklet"
-                      data-pin-href="https://www.pinterest.com/pin/create/button/"
-                      >Pinterest</a
-                    >
+                  <div class="contact-block">
+                    <div id="cmsmasters_icon_6fe373ojdc" class="cmsmasters_icon_wrap">
+                      <span class="fa-solid fa-envelope"></span>
+                      <span class="cmsmasters_simple_icon_title"></span>
+                    </div>
+                    <div class="text-box">
+                      <h5>Email Address:</h5>
+                      <p>exampleinfo@gmail.com</p>
+                    </div>
                   </div>
-                  <div class="share_wrap">
-                    <a
-                      href="https://www.facebook.com/sharer/sharer.php?display=popup&amp;u=https%3A%2F%2Fnexus.uiparadox.com%2F"
-                      class="button cmsmasters-icon-facebook"
-                      >Facebook</a
-                    >
+                  <div class="contact-block">
+                    <div id="cmsmasters_icon_7pu6fe3fms" class="cmsmasters_icon_wrap">
+                      <span class="fa-solid fa-location-dot"></span>
+                      <span class="cmsmasters_simple_icon_title"></span>
+                    </div>
+                    <div class="text-box">
+                      <h5>Address:</h5>
+                      <p>26 South Street, New York, NY 98, USA.</p>
+                      <p>&nbsp;</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div id="cmsmasters_column_8yewr13vm" class="cmsmasters_column one_half">
-              <div class="cmsmasters_column_inner">
-                <div class="cmsmasters_contact_form">
-                  <div
-                    class="wpcf7 js"
-                    id="wpcf7-f6-p1563-o1"
-                    lang="en-US"
-                    dir="ltr"
-                    data-wpcf7-id="6"
+              <div
+                id="cmsmasters_heading_zeqwe011o"
+                class="cmsmasters_heading_wrap cmsmasters_heading_align_left"
+              >
+                <h5 class="cmsmasters_heading">Social Media:</h5>
+              </div>
+              <div class="cmsmasters_sharing">
+                <div class="share_wrap">
+                  <a
+                    data-pin-do="buttonBookmark"
+                    data-pin-custom="true"
+                    class="button cmsmasters_pinterest_button cmsmasters-icon-pinterest"
+                    data-pin-log="button_pinit_bookmarklet"
+                    data-pin-href="https://www.pinterest.com/pin/create/button/"
+                    >Pinterest</a
                   >
-                    <div class="screen-reader-response">
-                      <p role="status" aria-live="polite" aria-atomic="true"></p>
-                      <ul></ul>
-                    </div>
-                    <form
-                      action="/#wpcf7-f6-p1563-o1"
-                      method="post"
-                      class="wpcf7-form init"
-                      aria-label="Contact form"
-                      novalidate="novalidate"
-                      data-status="init"
-                    >
-                      <div style="display: none">
-                        <input type="hidden" name="_wpcf7" value="6" />
-                        <input type="hidden" name="_wpcf7_version" value="6.0.6" />
-                        <input type="hidden" name="_wpcf7_locale" value="en_US" />
-                        <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f6-p1563-o1" />
-                        <input type="hidden" name="_wpcf7_container_post" value="1563" />
-                        <input type="hidden" name="_wpcf7_posted_data_hash" value="" />
-                      </div>
-                      <p>
-                        <label>
-                          Your name<br />
-                          <span class="wpcf7-form-control-wrap" data-name="your-name"
-                            ><input
-                              size="40"
-                              maxlength="400"
-                              class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
-                              autocomplete="name"
-                              aria-required="true"
-                              aria-invalid="false"
-                              value=""
-                              type="text"
-                              name="your-name"
-                          /></span>
-                        </label>
-                      </p>
-                      <p>
-                        <label>
-                          Your email<br />
-                          <span class="wpcf7-form-control-wrap" data-name="your-email"
-                            ><input
-                              size="40"
-                              maxlength="400"
-                              class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
-                              autocomplete="email"
-                              aria-required="true"
-                              aria-invalid="false"
-                              value=""
-                              type="email"
-                              name="your-email"
-                          /></span>
-                        </label>
-                      </p>
-                      <p>
-                        <label>
-                          Subject<br />
-                          <span class="wpcf7-form-control-wrap" data-name="your-subject"
-                            ><input
-                              size="40"
-                              maxlength="400"
-                              class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
-                              aria-required="true"
-                              aria-invalid="false"
-                              value=""
-                              type="text"
-                              name="your-subject"
-                          /></span>
-                        </label>
-                      </p>
-                      <p>
-                        <label>
-                          Your message (optional)<br />
-                          <span class="wpcf7-form-control-wrap" data-name="your-message">
-                            <textarea
-                              cols="40"
-                              rows="10"
-                              maxlength="2000"
-                              class="wpcf7-form-control wpcf7-textarea"
-                              aria-invalid="false"
-                              name="your-message"
-                            ></textarea>
-                          </span>
-                        </label>
-                      </p>
-                      <p>
-                        <input
-                          class="wpcf7-form-control wpcf7-submit has-spinner"
-                          type="submit"
-                          value="Submit"
-                        /><span class="wpcf7-spinner"></span>
-                      </p>
-                      <div class="wpcf7-response-output" aria-hidden="true"></div>
-                    </form>
+                </div>
+                <div class="share_wrap">
+                  <a
+                    href="https://www.facebook.com/sharer/sharer.php?display=popup&amp;u=https%3A%2F%2Fnexus.uiparadox.com%2F"
+                    class="button cmsmasters-icon-facebook"
+                    >Facebook</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="11">
+          <div id="cmsmasters_column_8yewr13vm" class="cmsmasters_column">
+            <div class="cmsmasters_column_inner">
+              <div class="cmsmasters_contact_form">
+                <div
+                  class="wpcf7 js"
+                  id="wpcf7-f6-p1563-o1"
+                  lang="en-US"
+                  dir="ltr"
+                  data-wpcf7-id="6"
+                >
+                  <div class="screen-reader-response">
+                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                    <ul></ul>
                   </div>
+                  <form
+                    action="/#wpcf7-f6-p1563-o1"
+                    method="post"
+                    class="wpcf7-form init"
+                    aria-label="Contact form"
+                    novalidate="novalidate"
+                    data-status="init"
+                  >
+                    <div style="display: none">
+                      <input type="hidden" name="_wpcf7" value="6" />
+                      <input type="hidden" name="_wpcf7_version" value="6.0.6" />
+                      <input type="hidden" name="_wpcf7_locale" value="en_US" />
+                      <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f6-p1563-o1" />
+                      <input type="hidden" name="_wpcf7_container_post" value="1563" />
+                      <input type="hidden" name="_wpcf7_posted_data_hash" value="" />
+                    </div>
+                    <p>
+                      <label>
+                        Your name<br />
+                        <span class="wpcf7-form-control-wrap" data-name="your-name"
+                          ><input
+                            size="40"
+                            maxlength="400"
+                            class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                            autocomplete="name"
+                            aria-required="true"
+                            aria-invalid="false"
+                            value=""
+                            type="text"
+                            name="your-name"
+                        /></span>
+                      </label>
+                    </p>
+                    <p>
+                      <label>
+                        Your email<br />
+                        <span class="wpcf7-form-control-wrap" data-name="your-email"
+                          ><input
+                            size="40"
+                            maxlength="400"
+                            class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email"
+                            autocomplete="email"
+                            aria-required="true"
+                            aria-invalid="false"
+                            value=""
+                            type="email"
+                            name="your-email"
+                        /></span>
+                      </label>
+                    </p>
+                    <p>
+                      <label>
+                        Subject<br />
+                        <span class="wpcf7-form-control-wrap" data-name="your-subject"
+                          ><input
+                            size="40"
+                            maxlength="400"
+                            class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                            aria-required="true"
+                            aria-invalid="false"
+                            value=""
+                            type="text"
+                            name="your-subject"
+                        /></span>
+                      </label>
+                    </p>
+                    <p>
+                      <label>
+                        Your message (optional)<br />
+                        <span class="wpcf7-form-control-wrap" data-name="your-message">
+                          <textarea
+                            cols="40"
+                            rows="10"
+                            maxlength="2000"
+                            class="wpcf7-form-control wpcf7-textarea"
+                            aria-invalid="false"
+                            name="your-message"
+                          ></textarea>
+                        </span>
+                      </label>
+                    </p>
+                    <p>
+                      <input
+                        class="wpcf7-form-control wpcf7-submit has-spinner"
+                        type="submit"
+                        value="Submit"
+                      /><span class="wpcf7-spinner"></span>
+                    </p>
+                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                  </form>
                 </div>
               </div>
             </div>
@@ -195,5 +195,26 @@
 </script>
 
 <style scoped lang="scss">
-//
+.title-row {
+  padding-top: 80px;
+  padding-bottom: 48px;
+}
+.content-row {
+  .el-col {
+    background: rgb(30, 31, 32);
+    margin: 0 1.2rem;
+  }
+}
+.cmsmasters_column {
+  padding: 28px;
+}
+
+.button,
+.wpcf7-submit {
+  transition: all 0.3s ease;
+}
+
+p {
+  color: #92949f;
+}
 </style>
